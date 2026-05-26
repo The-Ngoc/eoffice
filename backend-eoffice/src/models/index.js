44961d535e37c4695175772bf50a5494998739ad
Bomm <ngongoc0804@gmail.com>
2026-05-26 19:49:22 +0700

    f

--- backend-eoffice/src/models/index.js
+++ backend-eoffice/src/models/index.js
@@ -1,23 +1,52 @@
+'use strict';
+
 const fs = require('fs');
 const path = require('path');
-const sequelize = require('../config/db');
-
+const Sequelize = require('sequelize');
+const process = require('process');
 const basename = path.basename(__filename);
+
+// 1. Xác định môi trường hiện tại (Render gán NODE_ENV = production)
+const env = process.env.NODE_ENV || 'development';
+
+// 2. Trỏ đúng đường dẫn tới file config.json ở ngoài thư mục gốc
+const config = require(path.resolve('config', 'config.json'))[env];
 const db = {};
 
-fs.readdirSync(__dirname)
-    .filter((file) => file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js')
-    .forEach((file) => {
-        const model = require(path.join(__dirname, file));
-        db[model.name] = model;
-    });
+let sequelize;
 
-Object.keys(db).forEach((modelName) => {
-    if (typeof db[modelName].associate === 'function') {
-        db[modelName].associate(db);
-    }
+// 3. Nếu môi trường có cấu hình use_env_variable (như khối production ta đã làm)
+if (config && config.use_env_variable) {
+  // Bốc chuỗi connection từ biến DATABASE_URL trên Render
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  // Dùng cho máy local (development)
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+
+// --- ĐOẠN QUÉT MODEL DƯỚI ĐÂY GIỮ NGUYÊN THEO CODE CŨ CỦA BẠN ---
+fs
+  .readdirSync(__dirname)
+  .filter(file => {
+    return (
+      file.indexOf('.') !== 0 &&
+      file !== basename &&
+      file.slice(-3) === '.js' &&
+      file.indexOf('.test.js') === -1
+    );
+  })
+  .forEach(file => {
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
 });
 
 db.sequelize = sequelize;
+db.Sequelize = Sequelize;
 
 module.exports = db;
